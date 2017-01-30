f739f632f469311e0fef04b223d9b083f51de909
TomWPJ <tj14830@my.bristol.ac.uk>
2017-01-30 11:06:57 +0000

    ClosestIntersection() done

--- Source/skeleton.cpp
+++ Source/skeleton.cpp
@@ -19,8 +19,20 @@ int t;
 /* ----------------------------------------------------------------------------*/
 /* FUNCTIONS                                                                   */
 
+struct Intersection
+{
+	vec3 position;
+	float distance;
+	int triangleIndex;
+};
+
 void Update();
 void Draw();
+bool ClosestIntersection(
+	vec3 start,
+	vec3 dir,
+	const vector<Triangle>& triangles,
+	Intersection& closestIntersection );
 
 int main( int argc, char* argv[] )
 {
@@ -35,6 +47,31 @@ int main( int argc, char* argv[] )
 
 	SDL_SaveBMP( screen, "screenshot.bmp" );
 	return 0;
+}
+
+bool ClosestIntersection(vec3 start,vec3 dir,const vector<Triangle>& triangles,Intersection& closestIntersection)
+{
+	bool intersect = false;
+	for (u_int i = 0;i<triangles.size();i++){
+		Triangle triangle = triangles[i];
+		vec3 v0 = triangle.v0;
+		vec3 v1 = triangle.v1;
+		vec3 v2 = triangle.v2;
+		vec3 e1 = v1 - v0;
+		vec3 e2 = v2 - v0;
+		vec3 b = start - v0;
+		mat3 A( -dir, e1, e2 );
+		vec3 x = glm::inverse( A ) * b;
+		if (x.y > 0 && x.z > 0 && (x.y+x.z) < 1 && x.x >= 0){
+			if (x.x < closestIntersection.distance){
+				closestIntersection.distance = x.x;
+				closestIntersection.position = start+x.x*dir;
+				closestIntersection.triangleIndex = i;
+			}
+			intersect = true;
+		}
+	}
+	return intersect;
 }
 
 void Update()
