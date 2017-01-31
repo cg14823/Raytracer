ba6b41edff1958c3aeb1cdf53e0c9c1a63551b3b
Carlos Gonzalez <19charles96@gmail.com>
2017-01-31 11:16:37 +0000

    Fixed error in camera movement

--- Source/skeleton.cpp
+++ Source/skeleton.cpp
@@ -94,29 +94,50 @@ bool ClosestIntersection(vec3 start,vec3 dir,const vector<Triangle>& triangles,I
 		vec3 x = glm::inverse( A ) * b;
 		if (x.x < closestIntersection.distance){
 			if (x.y >= 0 && x.z >= 0 && (x.y+x.z) <= 1 && x.x >= 0){
-				if (closestIntersection.triangleIndex != i) {
-					closestIntersection.distance = x.x;
-					closestIntersection.position = start + x.x*dir;
-					closestIntersection.triangleIndex = i;
-					intersect = true;
-				}
+				closestIntersection.distance = x.x;
+				closestIntersection.position = start + x.x*dir;
+				closestIntersection.triangleIndex = i;
+				intersect = true;
 			}
 		}
 	}
 	return intersect;
 }
 
+bool intersect(vec3 start, vec3 dir, const vector<Triangle>& triangles, Intersection& closestIntersection)
+{
+	bool intersect = false;
+	for (int i = 0; i<triangles.size(); i++) {
+		
+		Triangle triangle = triangles[i];
+		vec3 e1 = triangle.v1 - triangle.v0;
+		vec3 e2 = triangle.v2 - triangle.v0;
+		vec3 b = start - triangle.v0;
+		mat3 A(-dir, e1, e2);
+		vec3 x = glm::inverse(A) * b;
+		if (x.x < closestIntersection.distance) {
+			closestIntersection.distance = x.x;
+			closestIntersection.position = start + x.x*dir;
+			closestIntersection.triangleIndex = i;
+			intersect = true;
+		}
+		
+	}
+	return intersect;
+}
+
 vec3 DirectLight(const Intersection& i){
+
 	float light2point = pow(i.position.x - lightPos.x,2) + pow(i.position.y - lightPos.y,2) + pow(i.position.z - lightPos.z,2);
 	Intersection j;
 	j.distance = m;
-	j.triangleIndex = i.triangleIndex;
-	if (ClosestIntersection(i.position, lightPos-i.position, triangles,j)) {
-		if (j.distance < sqrt(light2point)) {
+	if (intersect(i.position, i.position-lightPos, triangles,j)) {
+
+		//cout << "j: " << abs(j.distance) << " light2pos: " << sqrt(light2point) << endl;
+		if (abs(j.distance) < sqrt(light2point)) {
 			return vec3(0.0, 0.0, 0.0);
 		}
 	}
-	//cout << "h: " << j.distance << " light2pos: " << sqrt(light2point) << endl;
 	float A = ( 4 * 3.14159 * light2point);
 	vec3 B = lightColor/A;
 	float r  = glm::dot(triangles[i.triangleIndex].normal, glm::normalize(lightPos-i.position));
@@ -179,10 +200,6 @@ void Update()
 	}
 	if (keystate[SDLK_l]) {
 		lightPos.x += 0.1;
-	}
-	if (keystate[SDLK_j]) {
-		yaw -= 0.0174;
-		R = mat3(vec3(1, 0, yaw), vec3(0, 1, 0), vec3(-yaw, 0, 1));
 	}
 }
 
